db789a94f76c7c94efa21272e7fbfbcb5715ba56
e1turin <ivturnik2003@gmail.com>
2025-02-22 00:42:21 +0300

    find references shortcut

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -79,22 +79,25 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
+
     map('n', 'gD', vim.lsp.buf.declaration, opts)
     map('n', 'gd', vim.lsp.buf.definition, opts)
-    map('n', 'K', vim.lsp.buf.hover, opts)
+    map('n', 'gtd', vim.lsp.buf.type_definition, opts)
     map('n', 'gi', vim.lsp.buf.implementation, opts)
+
+    map('n', 'K', vim.lsp.buf.hover, opts)
     map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+    map({ 'n', 'v' }, '<leader>.', vim.lsp.buf.code_action, opts)
+    map('n', '<leader>r', vim.lsp.buf.rename, opts) -- fefactor rename
+    -- map('v', '<leader>f', function() -- I use Vgq
+    --   vim.lsp.buf.format { async = true }
+    -- end, opts)
+
     -- map('n', '<leader>cd', vim.lsp.buf.add_workspace_folder, opts) -- idk not use
     -- map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
     -- map('n', '<leader>wl', function()
       -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, opts)
-    map('n', 'gtd', vim.lsp.buf.type_definition, opts)
-    map('n', '<leader>rr', vim.lsp.buf.rename, opts) -- fefactor rename
-    map({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
-    map('n', 'gr', vim.lsp.buf.references, opts)
-    -- map('v', '<leader>f', function() -- I use Vgq
-    --   vim.lsp.buf.format { async = true }
     -- end, opts)
   end,
 })
